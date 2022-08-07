3abb9be3ce8d62082503ac5029110c7fb642d554
Nishal Gurung <nishal.gurung4@outlook.com>
2022-08-07 11:50:28 +0545

    publish products

--- ContosoCrafts/Pages/Index.cshtml.cs
+++ ContosoCrafts/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContosoCrafts.Models;
+using ContosoCrafts.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.Pages;
@@ -7,13 +9,23 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public JsonFileProductService ProductService { get; }
+
+    /**
+     * IEnumerable in C# is an interface that defines one method, GetEnumerator which returns an IEnumerator interface. 
+     * This allows readonly access to a collection then a collection that implements IEnumerable can be used with a for-each statement
+     **/
+    public IEnumerable<Product>? Products { get; private set; }
+
+    /**
+     * OnGet to handle GET Request
+     **/
+    public void OnGet() => Products = ProductService.GetProducts();
+
+    public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
     {
         _logger = logger;
-    }
-
-    public void OnGet()
-    {
+        ProductService = productService;
 
     }
 }
